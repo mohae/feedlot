f7e50461f51c8b39f2686b988f86a5015167a2d4
Joel Scoble <joel.scoble@outlook.com>
2014-06-19 18:46:38 -0500

    added centos support

--- ranchr/release.go
+++ ranchr/release.go
@@ -2,17 +2,25 @@ package ranchr
 
 import (
 	"errors"
-	_ "fmt"
+	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"net/url"
 	_ "os"
 	"strings"
+	"time"
 )
+
+func init() {
+	// Psuedo-random is fine here
+	rand.Seed(time.Now().UTC().UnixNano())
+}
 
 type isoer interface {
 	SetChecksum() string
 	SetURL() string
-	SetFilename() string
+	SetName() string
 }
 
 // Iso information. The Baseurl and ChecksumType are set during creation
@@ -20,7 +28,7 @@ type iso struct {
 	BaseURL      string
 	Checksum     string
 	ChecksumType string
-	Filename     string
+	Name	     string
 	URL          string
 }
 
@@ -42,21 +50,20 @@ type ubuntu struct {
 // error is saved to the setting variable. This will be reflected in the
 // resulting Packer template, which will render it unusable until it is fixed.
 func (u *ubuntu) SetISOInfo() error {
-	u.SetFilename()
-	if err := u.SetChecksum(); err != nil {
+	u.setName()
+	if err := u.setChecksum(); err != nil {
 		return err
 	}
-	u.SetURL()
+	u.setURL()
 
 	return nil
 }
 
-func (u *ubuntu) SetChecksum() error {
+func (u *ubuntu) setChecksum() error {
 	// Don't check for ReleaseFull existence since Release will also resolve for Ubuntu dl directories.
 	// if the last character of the base url isn't a /, add it
 	var page string
 	var err error
-
 	if page, err = getStringFromURL(u.BaseURL + u.Release + "/" + strings.ToUpper(u.ChecksumType) + "SUMS"); err != nil {
 		logger.Error(err.Error())
 		return err
@@ -71,9 +78,9 @@ func (u *ubuntu) SetChecksum() error {
 	return nil
 }
 
-func (u *ubuntu) SetURL() {
+func (u *ubuntu) setURL() {
 	// Its ok to use Release in the directory path because Release will resolve correctly, at the directory level, for Ubuntu.
-	u.URL = u.BaseURL + u.Release + "/" + u.Filename
+	u.URL = u.BaseURL + u.Release + "/" + u.Name
 
 	return
 }
@@ -83,25 +90,25 @@ func (u *ubuntu) SetURL() {
 //      checksumText image.isoname
 // Notes: \n separate lines
 //      since this is plain text processing we don't worry about runes
-func (u *ubuntu) findChecksum(s string) (string, error) {
-	if s == "" {
-		err := errors.New("the string passed to ubuntu.findChecksum(s string) was empty; unable to process request")
+func (u *ubuntu) findChecksum(isoName string) (string, error) {
+	if isoName == "" {
+		err := errors.New("the string passed to ubuntu.findChecksum(isoName string) was empty; unable to process request")
 		logger.Error(err.Error())
 		return "", err
 	}
-	pos := strings.Index(s, u.Filename)
+	pos := strings.Index(isoName, u.Name)
 	if pos <= 0 {
 		// if it wasn't found, there's a chance that there's an extension on the release number
 		// e.g. 12.04.4 instead of 12.04. This usually affects the LTS versions, I think.
 		// For this look for a line  that contains .iso.
 		// Substring the release string and explode it on '-'. Update isoName
-		pos = strings.Index(s, ".iso")
+		pos = strings.Index(isoName, ".iso")
 		if pos < 0 {
 			err := errors.New("Unable to find ISO information while looking for the release string on the Ubuntu checksums page.")
 			logger.Error(err.Error())
 			return "", err
 		}
-		tmpRel := s[:pos]
+		tmpRel := isoName[:pos]
 		tmpSl := strings.Split(tmpRel, "-")
 		if len(tmpSl) < 3 {
 			err := errors.New("Unable to parse release information on the Ubuntu checksum page.")
@@ -110,44 +117,186 @@ func (u *ubuntu) findChecksum(s string) (string, error) {
 		}
 
 		u.ReleaseFull = tmpSl[1]
-		u.SetFilename()
+		u.setName()
 
-		pos = strings.Index(s, u.Filename)
-		if pos <= 0 {
-			err := errors.New("Unable to retrieve checksum while looking for " + u.Filename + " on the Ubuntu checksums page.")
+		pos = strings.Index(isoName, u.Name)
+		if pos < 0 {
+			err := errors.New("Unable to retrieve checksum while looking for " + u.Name + " on the Ubuntu checksums page.")
 			logger.Error(err.Error())
 			return "", err
 		}
 	}
 
-/*	if len(s) < pos - 2 {
-		err := errors.New("Unable to retrieve checksum information for " + u.Filename)
+	if len(isoName) < pos - 2 {
+		err := errors.New("Unable to retrieve checksum information for " + u.Name)
 		logger.Error(err.Error())
 		return "", err
 	}
-*/
+
 	if pos - 66 < 1 {
-		u.Checksum = s[:pos-2]
+		u.Checksum = isoName[:pos-2]
 	} else {
-		u.Checksum = s[pos-66 : pos-2]
+		u.Checksum = isoName[pos-66 : pos-2]
 	}
 
 	return u.Checksum, nil
 }
 
-func (u *ubuntu) SetFilename() {
+func (u *ubuntu) setName() {
 	if u.ReleaseFull == "" {
-		u.Filename = "ubuntu-" + u.Release + "-" + u.Image + "-" + u.Arch + ".iso"
+		u.Name = "ubuntu-" + u.Release + "-" + u.Image + "-" + u.Arch + ".iso"
 	} else {
-		u.Filename = "ubuntu-" + u.ReleaseFull + "-" + u.Image + "-" + u.Arch + ".iso"
+		u.Name = "ubuntu-" + u.ReleaseFull + "-" + u.Image + "-" + u.Arch + ".iso"
 	}
 
 	return
 }
 
-type CentOS struct {
+type centOS struct {
 	release
 }
+
+// For CentOS, only the whole version is needed. The latest version will be pulled from the iso name
+var centOSMirrorListURL = "http://mirrorlist.centos.org/?release=%s&arch=%s&repo=os"
+// Sets the ISO information for a Packer template. If any error occurs, the
+// error is saved to the setting variable. This will be reflected in the
+// resulting Packer template, which will render it unusable until it is fixed.
+func (c *centOS) SetISOInfo() error {
+	if err := c.setBaseURL(); err != nil {
+		return err
+	}
+	if err := c.setChecksum(); err != nil {
+		return err
+	}
+	c.setURL()
+	return nil
+}
+
+func (c *centOS) setBaseURL() error {
+	// Uses the release and arch information to get the list of mirrors.
+	// Picks a mirror and uses that as the baseURL. This is only done if 
+	// the baseURL is empty. If there is a custom value, it is assumed that
+	// it represents the mirror that Rancher should use.
+	var err error
+	if c.BaseURL != "" {
+		logger.Debug("No changes made; centOS.BaseURL was already set to " + c.BaseURL)
+		return nil
+	}
+	if c.Arch == "" {
+		err = errors.New("Unable to set BaseURL information for CentOS because the Arch was not set.")
+		logger.Critical(err.Error())
+		return err
+	}
+	if c.Release == "" {
+		err = errors.New("Unable to set BaseURL information for CentOS because the Release was not set.")
+		logger.Critical(err.Error())
+		return err
+	}
+	// We only care about the version, not the release. The version release will
+	// always bring up the currently supported version.
+	version := strings.Split(c.Release, ".")
+	mirrorURL := fmt.Sprintf(centOSMirrorListURL, version[0], c.Arch)
+	var page string
+	if page, err = getStringFromURL(mirrorURL); err != nil {
+		logger.Error(err.Error())
+		return err
+	}	
+	mirrors := strings.Split(page, "\n")
+	mirrorCount := len(mirrors)
+	if mirrorCount <= 1 {
+		err = errors.New("Encountered unexpected results while processing mirror results from " + mirrorURL)
+		logger.Error(err.Error())
+		return err
+	}
+	// Use the URL provided by the list as the starting point
+	c.BaseURL = mirrors[rand.Intn(mirrorCount - 1)]
+	// But that url is not actually usable for our needs so get the URL structure
+	// and modify it so that it works for isos.
+	baseURL, err := url.Parse(c.BaseURL)
+	if err != nil {
+		return err
+	}
+	pathParts := strings.Split(baseURL.Path, "/")
+	l := len(pathParts)
+	path := ""
+	for i := 0; i < l - 4; i++ {
+		path += pathParts[i] + "/"
+	}
+	if path != "" {
+		baseURL.Path = trimSuffix(path, "/")
+	}
+	// Set the ReleaseFull 
+	c.ReleaseFull = pathParts[l-4]
+	c.BaseURL = baseURL.String()
+	c.setName()
+	return nil
+}
+
+func (c *centOS) setChecksum() error {
+	// Don't check for ReleaseFull existence since Release will also resolve for Ubuntu dl directories.
+	// if the last character of the base url isn't a /, add it
+	var page string
+	var err error
+	if page, err = getStringFromURL(c.BaseURL + "/" + c.ReleaseFull + "/isos/" + c.Arch + "/" + strings.ToLower(c.ChecksumType) + "sum.txt"); err != nil {
+		logger.Error(err.Error())
+		return err
+	}
+	// Now that we have a page...we need to find the checksum and set it
+	if c.Checksum, err = c.findChecksum(page); err != nil {
+		logger.Error(err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (c *centOS) setURL() {
+	// The release needs to be set to the current release, not the major version, this only
+	// applies if the number is a whole number.
+	releaseParts := strings.Split(c.Release, ".")
+	if len(releaseParts) == 1 {
+		// This is just the version, replace it with a release
+		urlParts := strings.Split(c.BaseURL, "/")
+		// The version is the 3rd from last
+		c.Release = urlParts[len(urlParts) - 1]
+	}
+	c.URL = c.BaseURL + "/" + c.ReleaseFull + "/isos/" + c.Arch + "/" + c.Name
+	return
+}
+
+func (c *centOS) findChecksum(page string) (string, error) {
+	// Finds the line in the incoming string with the isoName requested,
+	// strips out the checksum and returns it. This is for CentOS checksums
+	// which are in plaintext.
+	//      checksumText  image.isoname
+	// Notes: \n separate lines and two space separate the checksum and image name
+	//      since this is plain text processing we don't worry about runes
+	if page == "" {
+		err := errors.New("the string passed to centOS.findChecksum(s string) was empty; unable to process request")
+		logger.Error(err.Error())
+		return "", err
+	}
+	pos := strings.Index(page, c.Name)
+	if pos < 0 {
+		err := errors.New("Unable to find ISO information while looking for the release string on the CentOS checksums page.")
+		logger.Error(err.Error())
+		return "", err
+	}
+	tmpRel := page[:pos]
+	tmpSl := strings.Split(tmpRel, "\n")
+	// The checksum we want is the last element in the array
+	checksum := strings.TrimSpace(tmpSl[len(tmpSl)-1])
+	return checksum, nil
+}
+
+func (c *centOS) setName() {
+	if c.ReleaseFull == "" {
+		c.ReleaseFull = c.Release
+	}
+	c.Name = "CentOS-" + c.ReleaseFull + "-" + c.Arch + "-" + c.Image + ".iso"
+	return
+}
+
 
 func getStringFromURL(url string) (string, error) {
 	// Get the URL resource
--- ranchr/release_test.go
+++ ranchr/release_test.go
@@ -1,6 +1,8 @@
 package ranchr
 
 import (
+	"net/url"
+	"strings"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -12,92 +14,98 @@ func newTestUbuntu() ubuntu {
 	u.Image = "server"
 	u.Arch = "amd64"
 	u.ChecksumType = "sha256"
-	u.BaseURL = "http://releases.ubuntu.com/"
-	u.SetFilename()
-	u.SetURL()
-	
+	u.BaseURL = "http://releases.ubuntu.com/"	
 	return u
 }
 
-func TestSetISOInfo(t *testing.T) {
+func TestUbuntuSetISOInfo(t *testing.T) {
 	Convey("Given a new release object for Ubuntu", t, func() {
 		u := newTestUbuntu()
 		Convey("Set ISO info", func() {
-			if err := u.SetISOInfo(); err == nil {
-				Convey("The URL should be", func() {
-					So(u.URL, ShouldEqual, "http://releases.ubuntu.com/14.04/ubuntu-14.04-server-amd64.iso")
-				})
-				Convey("The Checksum should be", func() {
-					So(u.Checksum, ShouldEqual, "ababb88a492e08759fddcf4f05e5ccc58ec9d47fa37550d63931d0a5fa4f7388")
-				})
-				Convey("The Filename should be", func() {
-					So(u.Filename, ShouldEqual, "ubuntu-14.04-server-amd64.iso")
-				})
-			}
-			
+			err := u.SetISOInfo()
+			Convey("Should not result in an error", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("The URL should be", func() {
+				So(u.URL, ShouldEqual, "http://releases.ubuntu.com/14.04/ubuntu-14.04-server-amd64.iso")
+			})
+			Convey("The Checksum should be", func() {
+				So(u.Checksum, ShouldEqual, "ababb88a492e08759fddcf4f05e5ccc58ec9d47fa37550d63931d0a5fa4f7388")
+			})
+			Convey("The Name should be", func() {
+				So(u.Name, ShouldEqual, "ubuntu-14.04-server-amd64.iso")
+			})
 		})
-
 		Convey("Set ISO info, error", func() {
 			u.Release = ""
-			if err := u.SetISOInfo(); err != nil {
-				Convey("Attempt to set ISO information with bad values. The error should be", func() {
-					So(err.Error(), ShouldEqual, "Unable to find ISO information while looking for the release string on the Ubuntu checksums page.")
-				})
-			}
+			err := u.SetISOInfo();
+			Convey("Attempt to set ISO information with bad values. The error should be", func() {
+				So(err.Error(), ShouldEqual, "Unable to find ISO information while looking for the release string on the Ubuntu checksums page.")
+			})
 		})		
 	})
 }
 
-func TestSetChecksum(t *testing.T) {
+func TestUbuntuSetChecksum(t *testing.T) {
 	Convey("Given a supported distro struct", t, func() {
 		u := newTestUbuntu()
 		Convey("Given the ubuntu 14.04 information, check checksum retrieval is working.", func() {
-			if err := u.SetChecksum(); err == nil {
-				Convey("The set checksum should be ", func() {
-					So(u.Checksum, ShouldEqual, "ababb88a492e08759fddcf4f05e5ccc58ec9d47fa37550d63931d0a5fa4f7388")
-				})
-			}
+			err := u.setChecksum()
+			Convey("Setting the Checksum should not result in an error", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("The set checksum should be ", func() {
+				So(u.Checksum, ShouldEqual, "ababb88a492e08759fddcf4f05e5ccc58ec9d47fa37550d63931d0a5fa4f7388")
+			})
 		})
-
 		Convey("Check SetChecksum results with an error on getting url", func() {
 			u.ChecksumType =  "ABC"
 			u.BaseURL = "http://releasea.ubuntu.com"
-			if err := u.SetChecksum(); err != nil {
-				Convey("The error should be ", func() {
-					So(err.Error(), ShouldEqual, "Get http://releasea.ubuntu.com14.04/ABCSUMS: dial tcp: lookup releasea.ubuntu.com14.04: no such host")
-				})
-			}
+			err := u.setChecksum()
+			Convey("The error should be ", func() {
+				So(err.Error(), ShouldEqual, "Get http://releasea.ubuntu.com14.04/ABCSUMS: dial tcp: lookup releasea.ubuntu.com14.04: no such host")
 			})
-				
+		})
+		Convey("Calling checksum with an invalid filename but valid settings for an iso", func() {
+			u.Name =  "aslk"
+			err := u.setChecksum()
+			Convey("Should not error", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("Should result in a valid checksum as a result of the extra processing necessary to handle incrimental releases, e.g. 12.04.4 vs 12.04, which rebuilds the filename, resulting in a valid filename as long as the other settings are valid", func() {
+				So(u.Checksum, ShouldEqual, "ababb88a492e08759fddcf4f05e5ccc58ec9d47fa37550d63931d0a5fa4f7388")
+			})
+		})
 		Convey("Check SetChecksum results with an error on parsing url get results", func() {
-			u.Filename =  "aslk"
-			if err := u.SetChecksum(); err != nil {
-				Convey("The error should be ", func() {
-					So(err.Error(), ShouldNotEqual, "ssz")
-				})
-			}
+			// use at least 1 invalid setting
+			u.Image =  "random"
+			err := u.setChecksum()
+			Convey("The error should be ", func() {
+				So(err.Error(), ShouldEqual, "Unable to retrieve checksum while looking for ubuntu-14.04-random-amd64.iso on the Ubuntu checksums page.")
+			})
 		})
 	})
 }
 
-func TestSetURL(t *testing.T) {
+func TestUbuntuSetURL(t *testing.T) {
 	Convey("Given a supported distro struct", t, func() {
 		u := newTestUbuntu()
+		u.setName()
 		Convey("Setting the URL", func() {
-			u.SetURL()
+			u.setURL()
 			So(u.URL, ShouldEqual, "http://releases.ubuntu.com/14.04/ubuntu-14.04-server-amd64.iso")
 		})
 	})
 }
 
-func TestFindChecksum(t *testing.T) {
+func TestUbuntuFindChecksum(t *testing.T) {
 	Convey("Given a supported distro struct", t, func() {
 		var err error
 		var s string
 		u := newTestUbuntu()
 		Convey("Finding the checksum using an empty string to be searched", func() {
 			s, err = u.findChecksum("")
-			So(err.Error(), ShouldEqual, "the string passed to ubuntu.findChecksum(s string) was empty; unable to process request")
+			So(err.Error(), ShouldEqual, "the string passed to ubuntu.findChecksum(isoName string) was empty; unable to process request")
 			So(s, ShouldEqual, "")
 		})
 		Convey("And a results checksums page for the target iso", func() {
@@ -107,7 +115,7 @@ ababb88a492e08759fddcf4f05e5ccc58ec9d47fa37550d63931d0a5fa4f7388 *ubuntu-14.04-s
 85c738fefe7c9ff683f927c23f5aa82864866c2391aeb376abfec2dfc08ea873 *ubuntu-14.04-server-i386.iso
 bc3b20ad00f19d0169206af0df5a4186c61ed08812262c55dbca3b7b1f1c4a0b *wubi.exe`
 			Convey("Finding a an invalid release string", func() {
-				u.Filename = "ubuntu-14.03-whatever.iso"
+				u.Name = "ubuntu-14.03-whatever.iso"
 				u.Image = "whatever"
 				s, err = u.findChecksum(checksumPage)
 				So(err.Error(), ShouldEqual, "Unable to retrieve checksum while looking for ubuntu-14.04-whatever-amd64.iso on the Ubuntu checksums page.")
@@ -146,22 +154,24 @@ fbe7f159337551cc5ce9f0ff72acefef567f3dcd30750425287588c554978501 *ubuntu-12.04.4
 		})
 	})
 }
+
 /*
+getPAge testing?
 		Convey("Given a release of 14.04, image = server, arch = amd64", func() {
 			u := newTestUbuntu()
 
-			Convey("The Filename should be 'ubuntu-14.04-server-amd64.iso'", func() {
-				So(u.Filename, ShouldEqual, "ubuntu-14.04-server-amd64.iso")
-			})
+			Convey("The Name should be 'ubuntu-14.04-server-amd64.iso'", func() {
+				So(u.Name, ShouldEqual, "ubuntu-14.04-server-amd64.iso")
+			})S
 		})
 
 
 		Convey("Given a release full of 14.04.4, image = server, arch = amd64", func() {
 			u := newTestUbuntu()
 			u.ReleaseFull = "14.04.4"
-			u.SetFilename()
-			Convey("The Filename should be ", func() {
-				So(u.Filename, ShouldEqual, "ubuntu-14.04.4-server-amd64.iso")
+			u.SetName()
+			Convey("The Name should be ", func() {
+				So(u.Name, ShouldEqual, "ubuntu-14.04.4-server-amd64.iso")
 			})
 		})
 
@@ -250,3 +260,168 @@ fbe7f159337551cc5ce9f0ff72acefef567f3dcd30750425287588c554978501 *ubuntu-12.04.4
 	})
 }
 */
+
+func newTestCentOS() centOS {
+	c := centOS{}
+	c.Release = "6"
+	c.Image = "minimal"
+	c.Arch = "x86_64"
+	c.ChecksumType = "sha256"
+	return c
+}
+
+func TestCentOSSetURL(t *testing.T) {
+	Convey("Given a supported distro struct", t, func() {
+		c := newTestCentOS()
+		Convey("Setting the URL", func() {
+			// Since the baseurl is picked at random, to respect the mirrolist
+			// structure, just make sure it isn't empty.	
+			err := c.setBaseURL()
+			So(err, ShouldBeNil)
+			So(c.BaseURL, ShouldNotEqual, "")
+			// And release full should be set
+			So(c.ReleaseFull, ShouldNotEqual, "")
+			//Make sure the name is set
+			Convey("Setting the iso name", func() {
+				c.setName()
+				So(c.Name, ShouldEqual, "CentOS-" + c.ReleaseFull + "-" + c.Arch + "-minimal.iso")
+				Convey("Setting the BaseURL", func() {
+					c.setURL()
+					So(c.URL, ShouldNotEqual, "")
+				})
+			})
+		})
+	})
+}
+
+func TestCentOSFindChecksum(t *testing.T) {
+	Convey("Given a supported distro struct", t, func() {
+		var err error
+		var s string
+		c := newTestCentOS()
+		c.setBaseURL()
+		c.setName()
+		c.setURL()
+		Convey("Finding the checksum using an empty string to be searched", func() {
+			s, err = c.findChecksum("")
+			So(err.Error(), ShouldEqual, "the string passed to centOS.findChecksum(s string) was empty; unable to process request")
+			So(s, ShouldEqual, "")
+		})
+		Convey("And a results checksums page for the target iso", func() {
+			checksumPage := `c796ab378319393f47b29acd8ceaf21e1f48439570657945226db61702a4a2a1  CentOS-6.5-x86_64-bin-DVD1.iso
+afd2fc37e1597c64b3c3464083c0022f436757085d9916350fb8310467123f77  CentOS-6.5-x86_64-bin-DVD2.iso
+58b40b26415133ed2af8e2f53b73b5f2aa013723742ce17671b5bb1880a20a99  CentOS-6.5-x86_64-LiveCD.iso
+e3efa9a6ca6f58ac4be0a6cdb09cc4f19125040124e1c162bc5cfef26a8926f0  CentOS-6.5-x86_64-LiveDVD.iso
+f9d84907d77df62017944cb23cab66305e94ee6ae6c1126415b81cc5e999bdd0  CentOS-6.5-x86_64-minimal.iso
+d8aaf698408c0c01843446da4a20b1ac03d27f87aad3b3b7b7f42c6163be83b9  CentOS-6.5-x86_64-netinstall.iso`
+			Convey("Finding a an invalid release string", func() {
+				c.Name = "CentOS-6.5-whatever.iso"
+				c.Image = "whatever"
+				s, err = c.findChecksum(checksumPage)
+				So(err.Error(), ShouldEqual, "Unable to find ISO information while looking for the release string on the CentOS checksums page.")
+				So(s, ShouldEqual, "")
+			})
+		})
+		Convey("And a results checksums page for the target iso", func() {
+			checksumPage := `c796ab378319393f47b29acd8ceaf21e1f48439570657945226db61702a4a2a1  CentOS-6.5-x86_64-bin-DVD1.iso
+afd2fc37e1597c64b3c3464083c0022f436757085d9916350fb8310467123f77  CentOS-6.5-x86_64-bin-DVD2.iso
+58b40b26415133ed2af8e2f53b73b5f2aa013723742ce17671b5bb1880a20a99  CentOS-6.5-x86_64-LiveCD.iso
+e3efa9a6ca6f58ac4be0a6cdb09cc4f19125040124e1c162bc5cfef26a8926f0  CentOS-6.5-x86_64-LiveDVD.iso
+f9d84907d77df62017944cb23cab66305e94ee6ae6c1126415b81cc5e999bdd0  CentOS-6.5-x86_64-minimal.iso
+d8aaf698408c0c01843446da4a20b1ac03d27f87aad3b3b7b7f42c6163be83b9  CentOS-6.5-x86_64-netinstall.iso`
+			Convey("Finding a valid release string", func() {
+				c.Release = "6"
+				c.ReleaseFull = "6.5"
+				c.Image = "minimal"
+				c.Name = "CentOS-6.5-x86_64-minimal.iso"
+				s, err = c.findChecksum(checksumPage)
+				So(err, ShouldBeNil)
+				So(s, ShouldEqual, "f9d84907d77df62017944cb23cab66305e94ee6ae6c1126415b81cc5e999bdd0")
+			})
+		})
+	})
+}
+
+func TestCentOSSetISOInfo(t *testing.T) {
+	Convey("Given a new release object for CentOS", t, func() {
+		c := newTestCentOS()
+		Convey("Set ISO info", func() {	
+			err := c.SetISOInfo()
+			Convey("Should not result in an  error", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("The URL should be", func() {
+				So(c.URL, ShouldNotEqual, "")
+				Convey("And it should be a valid url", func(){
+					u, err := url.Parse(c.URL)
+					So(err, ShouldBeNil)
+					So(u.Scheme, ShouldEqual, "http")
+					So(u.Host, ShouldNotEqual, "")
+					parts :=  strings.Split(u.Path,"/")
+					l := len(parts)
+					So(parts[l-4], ShouldEqual, "6.5")
+					So(parts[l-3], ShouldEqual, "isos")
+					So(parts[l-2], ShouldEqual, "x86_64")
+				})
+			})
+			Convey("The BaseURL should exist", func() {
+				// Since this is random we just check that scheme, host, and path exist
+				So(c.BaseURL, ShouldNotEqual, "")
+				Convey("And it should be a valid url", func() {
+					u, err := url.Parse(c.BaseURL)
+					So(err, ShouldBeNil)
+					So(u.Scheme, ShouldNotEqual, "")
+					So(u.Scheme, ShouldNotEqual, "ftp")
+					So(u.Host, ShouldNotEqual, "")
+					So(u.Path, ShouldNotEqual,"")
+				})
+			})
+			Convey("The Checksum should be", func() {
+				So(c.Checksum, ShouldEqual, "f9d84907d77df62017944cb23cab66305e94ee6ae6c1126415b81cc5e999bdd0")
+			})
+			Convey("The Name should be", func() {
+				So(c.Name, ShouldEqual, "CentOS-6.5-x86_64-minimal.iso")
+			})
+		})
+		Convey("Set ISO info, error", func() {
+			c.Release = ""
+			err := c.SetISOInfo()
+			Convey("Attempt to set ISO information with bad values. The error should be", func() {
+				So(err.Error(), ShouldEqual, "Unable to set BaseURL information for CentOS because the Release was not set.")
+			})
+		})		
+	})
+}
+
+
+func TestCentOSSetChecksum(t *testing.T) {
+	Convey("Given a supported distro struct", t, func() {
+		c := newTestCentOS()
+		c.setBaseURL()
+		c.setURL()
+		Convey("Given the CentOS 6 information, check checksum retrieval is working.", func() {
+			err := c.setChecksum()
+			Convey("Should not result in an error", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("The set checksum should be ", func() {
+				So(c.Checksum, ShouldEqual, "f9d84907d77df62017944cb23cab66305e94ee6ae6c1126415b81cc5e999bdd0")
+			})
+		})
+		Convey("Check SetChecksum results with an error on getting url", func() {
+			c.ChecksumType =  "ABC"
+			c.BaseURL = "http://adfarfawer.com/notaurl"
+			err := c.setChecksum()
+			Convey("The error should be ", func() {
+				So(err.Error(), ShouldEqual, "Get http://adfarfawer.com/notaurl/" + c.ReleaseFull + "/isos/" + c.Arch + "/abcsum.txt: dial tcp: lookup adfarfawer.com: no such host")
+			})
+		})		
+		Convey("Check SetChecksum results with an error on parsing url get results", func() {
+			c.Name =  "aslk"
+			err := c.setChecksum()
+			Convey("The error should be ", func() {
+				So(err.Error(), ShouldNotEqual, "ssz")
+			})
+		})
+	})
+}
