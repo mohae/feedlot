f914ba1d54f9f1fc8798eb321a202583da535a64
Joel <mohae@users.noreply.github.com>
2014-07-15 13:44:26 -0500

    Update setup.sh

--- src/ubuntu/scripts/setup.sh
+++ src/ubuntu/scripts/setup.sh
@@ -2,5 +2,4 @@
 # Update the system and install basics
 apt-get update -y -q > /dev/null
 apt-get upgrade -y -q > /dev/null
-apt-get install -y -q curl wget git vim rsync openssh sudo
-apt-get intsall -y gcc make gcc-c++ kernel-devel-`uname-r`
+apt-get install -y -q curl wget git vim rsync sudo
