a02ae85a07c6978fdd8ef581a8688d8fb17a11ee
Joel Scoble <joel.scoble@outlook.com>
2015-11-20 13:33:12 -0600

    remove examples in prep for redoing subtree; I'm taking lazy way out because this is easier than resolving the conflicts as I know I want the remote as is and not what is currently in examples

--- examples/packer_sources/centos/shell/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Update the system and install basics
-yum update -y -q > /dev/null
-yum upgrade -y -q > /dev/null
-yum install -y -q curl wget git vim rsync openssh sudo
-yum install -y gcc make gcc-c++ kernel-devel-`uname-r`
--- examples/packer_sources/ubuntu/shell/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-apt-get update
-apt-get upgrade -y
-apt-get -y install linux-headers-$(uname -r)
-<<<<<<< HEAD
-apt-get install -y curl wget rsync sudo
-=======
-apt-get install -y curl wget rsync
->>>>>>> c5c1889d741c448ab595f6ebdcddfd5acc59b2d8
